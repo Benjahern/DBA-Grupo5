0179d86bc6146b7d139fb10364d4094ed6e7f92a
Mharko8849 <marco.ortiz@usach.cl>
2026-04-19 21:26:38 -0400

    Me falto añadir el Backend

---
+++ LAB1/Backend/src/test/java/Host_Usach_Cloud/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Host_Usach_Cloud.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
